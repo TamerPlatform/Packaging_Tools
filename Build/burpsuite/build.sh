115beff6950fb32f6863097672b3abd55e592aea
Anant Shrivastava <anant@anantshri.info>
2016-05-21 05:25:40 +0530

    burpsuite initial commit

---
+++ Build/burpsuite/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+MAINVER="1.7.01beta"
+Extra=""
+VERSION=$MAINVER$Extra
+#mkdir source
+wget "https://portswigger.net/DownloadUpdate.ashx?Product=Free" -O source/burpsuite-free.jar
+wget "https://portswigger.net/burp/eula-free.html" -O source/license.html
+html2text "https://portswigger.net/burp/eula-free.html" | tee source/license.txt
+rm -rf usr opt
+mkdir -p usr/bin opt/burpsuite/
+cp source/burpsuite-free.jar opt/burpsuite/
+cp source/license.txt opt/burpsuite/license.txt
+cp burpsuite.png usr/share/icons/androidtamer/burpsuite-free.png 
+cat <<EOF > usr/bin/burpsuite-free
+#!/bin/bash
+java -jar /opt/burpsuite/burpsuite-free.jar "\$@"
+EOF
+cat <<EOF > usr/share/applications/burpsuite-free.desktop
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon[en_US]=/usr/share/icons/androidtamer/burpsuite-free.png
+Name[en_US]=BurpSuite-Free
+Exec=/usr/bin/x-terminal-emulator --command "/usr/bin/jd-gui; \$SHELL"
+Name=BurpSuite-Free
+Icon=/usr/share/icons/androidtamer/jd-gui.png
+Categories=X-tamer-pentest
+EOF
+chmod 755 usr/bin/burpsuite-free
+debctrl "burpsuite-free" "$VERSION" "Burp Suite Free\n is a platform for performing security testing of web applications.\n Its various tools work seamlessly together to support the entire testing process,\n from initial mapping and analysis of an application's attack surface,\n through to finding and exploiting security vulnerabilities.\n Burp gives you full control, letting you combine advanced manual techniques\n with state-of-the-art automation, to make your work faster,\n more effective, and more fun." "http://portswigger.net/index.html" "all" "default-jre"
+changelog
+build_package usr opt
