1e851b70d72ba52bb0fc24724b75ff28f85e2d6a
Anant Shrivastava <anant@anantshri.info>
2016-06-05 21:46:01 +0530

    dex-oracle gem package

---
+++ Build/dex-oracle/build.sh
@@ -0,0 +1,2 @@
+fpm -s gem -t deb --category AndroidTamer -m "Anant Shrivastava <anant@anantshri.info>" --vendor AndroidTamer rubyzip
+fpm -s gem -t deb --category AndroidTamer -m "Anant Shrivastava <anant@anantshri.info>" --vendor AndroidTamer dex-oracle
