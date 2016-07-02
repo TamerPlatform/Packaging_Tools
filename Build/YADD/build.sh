9fb866d0dd250c158ddff5d1567aab403a852f96
Anant Shrivastava <anant@anantshri.info>
2016-07-02 20:13:34 +0530

    correct name is YADD

--- Build/YADD/build.sh
+++ Build/YADD/build.sh
@@ -29,21 +29,21 @@ then
 fi
 cd ..
 mkdir -p usr/bin usr/share/applications
-cp source/bin/dumper usr/bin/dumper
-chmod 755 usr/bin/dumper
-cat <<EOF > usr/share/applications/dumper.desktop
+cp source/bin/dumper usr/bin/yadd-dumper
+chmod 755 usr/bin/yadd-dumper
+cat <<EOF > usr/share/applications/yadd-dumper.desktop
 #!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
-TryExec=/usr/bin/dumper
-Exec=x-terminal-emulator --command "dumper --help; $SHELL"
+TryExec=/usr/bin/yadd-dumper
+Exec=x-terminal-emulator --command "yadd-dumper --help; $SHELL"
 Name=YAAD-dumper
 Description: Yet another Android Dex bytecode Disassembler
 Icon=terminator
 Categories=X-tamer-manualanalysis
 EOF
-debctrl "yaad-dumper" "$VERSION" "Yet another Android Dex bytecode Disassembler\n YADD is planed to be a complex disassembler for the Android Dex bytecode\n hat is, a hybrid tool to support pure binary/signature dumping and to provide an interface for reversing analysis" "https://github.com/AndroidTamer/YAAD" "all" "python"
+debctrl "yadd-dumper" "$VERSION" "Yet another Android Dex bytecode Disassembler\n YADD is planed to be a complex disassembler for the Android Dex bytecode\n hat is, a hybrid tool to support pure binary/signature dumping and to provide an interface for reversing analysis" "https://github.com/AndroidTamer/YADD" "all" "python"
 changelog
 build_package usr
