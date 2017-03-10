b52706777c1d9d56ee6957c2ea0063ee9d42cdaa
Anant Shrivastava <anant@anantshri.info>
2017-03-10 16:02:29 +0530

    finally drozer compile naile

--- Build/drozer/build.sh
+++ Build/drozer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MAINVER="2.4.2"
+MAINVER="2.4.3"
 Extra=""
 if [ -d "source" ]
 	then
@@ -9,5 +9,13 @@ if [ -d "source" ]
 else
 	git clone https://github.com/AndroidTamer/drozer ./source
 fi
-wget "https://github.com/mwrlabs/drozer/releases/download/$MAINVER/drozer-$MAINVER-py2.7.egg" -O "drozer-$MAINVER-py2.7.egg"
+echo "source as of now needs to be manually edited"
+echo 'install_requires = ["protobuf>=2.6.1","pyopenssl>=16.2.0", "pyyaml>=3.12"],'
+read -p "Please edit sources and once done press enter" test
+cd source
+sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+make apks
+cd ..
+sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
+#wget "https://github.com/mwrlabs/drozer/releases/download/$MAINVER/drozer-$MAINVER-py2.7.egg" -O "drozer-$MAINVER-py2.7.egg"
 build_pip local source/setup.py
