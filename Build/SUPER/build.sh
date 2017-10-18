aef99ac19379f309b3779201f88aa3446c399570
Anant Shrivastava <anant@anantshri.info>
2017-10-18 22:42:20 +0530

    SUPER Upgraded to 0.4.1

--- Build/SUPER/build.sh
+++ Build/SUPER/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER="0.4.0"
+VER="0.4.1"
 wget "https://github.com/SUPERAndroidAnalyzer/super/releases/download/"$VER"/super-analyzer_"$VER"_debian_amd64.deb"
 sign_only "super-analyzer_"$VER"_debian_amd64.deb"
 echo "DEB Signed and ready for usage"
